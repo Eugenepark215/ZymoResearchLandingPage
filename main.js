7ca73cb7f1a0d353c91c271a81a08a3ae55a4e6d
Eugene Park <epark215@gmail.com>
2023-11-19 17:06:46 -0800

    created rough structure and dom function

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ const $comments = document.querySelector('#comments');
 const $success = document.querySelector('#success');
 const $sub = document.querySelector('.sub');
 
+// const data = [
+//   {
+//     img: './icons/checkmark_icon.jpg',
+//     name: 'Lorem Ipsum',
+//     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//     link: 'https://www.lipsum.com/'
+//  },
+//   {
+//     img: './icons/bullseye_icon.jpg',
+//     name: 'Lorem Ipsum',
+//     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//     link: 'https://www.lipsum.com/'
+//   },
+//   {
+//     img: './icons/graphs_icon.jpg',
+//     name: 'Lorem Ipsum',
+//     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//     link: 'https://www.lipsum.com/'
+//   },
+// ]
+
 function handleSubmit(event) {
   event.preventDefault();
   const formValue = {
@@ -35,3 +56,37 @@ function handleSubmit(event) {
 }
 
 $form.addEventListener('submit', handleSubmit);
+
+function renderProductCard(entry) {
+  const $li = document.createElement('li');
+
+  const $productImgContainer = document.createElement('div');
+  $productImgContainer.setAttribute('class', 'product-img-container');
+  $li.appendChild($productImgContainer);
+
+  const $img = document.createElement('img');
+  $img.setAttribute('src', './icons/checkmark_icon.jpg');
+  $img.setAttribute('class', 'product-img');
+  $productImgContainer.appendChild($productImg)
+
+  const $productTextContainer = document.createElement('div');
+  $productTextContainer.setAttribute('class', 'product-text-container');
+  $li.appendChild($productTextContainer);
+
+  const $h1 = document.createElement('h1');
+  $h1.setAttribute('class', 'product-header');
+  $h1.textContent = 'Lorem Ipsum';
+  $productTextContainer.appendChild($h1);
+
+  const $p = document.createElement('p');
+  $p.setAttribute('class', 'product-text');
+  $h1.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,';
+  $productTextContainer.appendChild($p);
+
+  const $a = document.createElement('a');
+  $a.setAttribute('class', 'product-anchor');
+  $a.setAttribute('href', 'https://www.lipsum.com/')
+  $productTextContainer.appendChild($a);
+
+  return $li;
+}
